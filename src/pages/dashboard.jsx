cd64289986f0257a7130ce327f240ba332eca068
HolllD <iagogaraujo@gmail.com>
2021-05-09 18:17:09 -0300

    dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,16 +1,114 @@
-import React from "react";
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+//import
+import React from 'react';
+
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import { Typography } from '@material-ui/core';
+import Container from '@material-ui/core/Container'
+import Paper from '@material-ui/core/Paper';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        paper: {
+            padding: theme.spacing(2),
+            margin: 'auto',
+            maxWidth: 500,
+        },
+        image: {
+            width: 128,
+            height: 128,
+        },
+        img: {
+            margin: 'auto',
+            display: 'block',
+            maxWidth: '100%',
+            maxHeight: '100%',
+        },
+    }),
+);
 
 // Página para login
 
+function createData(time, name, item_ID, amount) {
+    return { time, name, item_ID, amount };
+}
+
+function preventDefault(event) {
+    event.preventDefault();
+}
+
+
+// MockData só para servir de exemplo
+const MockData = [
+    createData(0, '15-Jan-2021', 'Patricia Mullins', '98', '3'),
+    createData(1, '12-Jan-2021', 'Barbara Harris', '32', '2'),
+    createData(2, '08-Out-2020', 'Alan Peterson', '52', '5'),
+    createData(3, '02-Set-2019', 'Harold Powell', '18', '1'),
+    createData(4, '20-Dez-2018', 'Gerald Jenkins', '58', '9'),
+    createData(0, '15-Jan-2021', 'Patricia Mullins', '98', '3'),
+    createData(1, '12-Jan-2021', 'Barbara Harris', '32', '2'),
+    createData(2, '08-Out-2020', 'Alan Peterson', '52', '5'),
+    createData(3, '02-Set-2019', 'Harold Powell', '18', '1'),
+    createData(4, '20-Dez-2018', 'Gerald Jenkins', '58', '9'),
+    createData(0, '15-Jan-2021', 'Patricia Mullins', '98', '3'),
+    createData(1, '12-Jan-2021', 'Barbara Harris', '32', '2'),
+    createData(2, '08-Out-2020', 'Alan Peterson', '52', '5'),
+    createData(3, '02-Set-2019', 'Harold Powell', '18', '1'),
+    createData(4, '20-Dez-2018', 'Gerald Jenkins', '58', '9'),
+    createData(0, '15-Jan-2021', 'Patricia Mullins', '98', '3'),
+    createData(1, '12-Jan-2021', 'Barbara Harris', '32', '2'),
+    createData(2, '08-Out-2020', 'Alan Peterson', '52', '5'),
+    createData(3, '02-Set-2019', 'Harold Powell', '18', '1'),
+    createData(4, '20-Dez-2018', 'Gerald Jenkins', '58', '9'),
+];
+
 const Dashboard = () => {
-    return(
-        <div classname="App">
-            <h3>Ohayou</h3>
-        </div>
+
+    return (
+        <header className="App-header">
+        <React.Fragment>
+            <Container>
+                <Paper>
+                <Typography align="center" color="Black" variant="h4">Exemplo de tabela</Typography>
+                <Table size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell>Data</TableCell>
+                            <TableCell>Nome</TableCell>
+                            <TableCell>ID do produto</TableCell>
+                            <TableCell>Quantidade</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {MockData.map((MockData) => (
+                            <TableRow key={MockData.id}>
+                                <TableCell>{MockData.time}</TableCell>
+                                <TableCell>{MockData.name}</TableCell>
+                                <TableCell>{MockData.item_ID}</TableCell>
+                                <TableCell>{MockData.amount}</TableCell>
+                                <TableCell align="right">{MockData.amount}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <div>
+                </div>
+                </Paper>
+            </Container>
+        </React.Fragment>
+        </header>
     );
 }
 
